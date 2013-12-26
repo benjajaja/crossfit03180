b2e53eb0404e310d8fed557c68b8d696d5d83b13
alahin <alahin66@hotmail.com>
2013-12-26 17:07:06 +0100

    update calendar

--- calendario/calendario.php
+++ calendario/calendario.php
@@ -2,12 +2,12 @@
 <head>
 	<meta charset="utf-8">
 	<title>Crossfit03180</title>
-	<link rel="stylesheet" href="css/style_calendario.css"/>
-	<link rel="stylesheet" href="css/bootstrap.min_calendar.css"/>
-	<script src="js/jquery-1.9.1.js"></script>
-	<script src="js/jquery-ui.js"></script>
-	<script src="js/moment.min.js"></script>
-	<script src="js/bootstrap.min_calendar.js"></script>
+	<link rel="stylesheet" href="../css/style_calendario.css"/>
+	<link rel="stylesheet" href="../css/bootstrap.min_calendar.css"/>
+	<script src="../js/jquery-1.9.1.js"></script>
+	<script src="../js/jquery-ui.js"></script>
+	<script src="../js/moment.min.js"></script>
+	<script src="../js/bootstrap.min_calendar.js"></script>
 	
 	<script type="text/javascript">
 		$(function(){
