be46309a095e3810d59a16ce5228c3621e7506dd
benja <benja@maletin.(none)>
2014-01-02 21:15:37 +0100

    use db to authenticate a user
    modificaciones necesarias:
     alter table usuarios modify pass binary(20);
    
    para insertar un usuario:
     insert into usuarios set nombre = 'Benja', email = 'ste3ls@gmail.com', pass = UNHEX(SHA1('supersecreto007hola'));
    donde "hola" es la contraseña

--- api/index.php
+++ api/index.php
@@ -1,33 +1,57 @@
 <?php
 
-include('../config.php');
+$config = require('../config.php');
 
-/*function __autoload($class) {
-	return require('routes/' . $class . '.php');
-}*/
+require('adodb_lite/adodb.inc.php');
+$db = &ADONewConnection('mysql');
 
-require_once(__DIR__ . '/zaphpa/zaphpa.lib.php');
+if (!$db->Connect($config['db']['host'], $config['db']['user'], $config['db']['password'], $config['db']['database'])) {
+	header($_SERVER['SERVER_PROTOCOL'] . ' 503 Service not available', true, 503);
+	header('Content-Type: text/plain');
+	die('service not available. please try again later');
+}
 
+$db->_salt = $config['db']['salt']; // sal para encriptar bien
+
+$GLOBALS['db'] = $db; // esto es una mierda, las cosas no se deben meter variables globales NUNCA, pero me la suda pa esta web :D
+
+require(__DIR__ . '/zaphpa/zaphpa.lib.php');
 $router = new Zaphpa_Router();
 
-$addRoute = function($path, $class, $method) use ($router) {
+// cookie monster
+session_name('crossfit');
+session_start();
+
+$addRoute = function($path, $class, $methods, $handlers = null) use ($db, $router) {
 	$route = array(
-		'path' => '/api' . $path,
-		'file' => 'routes/' . $class . '.php'
-	);
-	$route[$method] = array($class, $method);
+		'path' => '/api' . $path, // la ruta de la URL, ej. /api/auth
+		'file' => 'routes/' . $class . '.php' // el fichero de la clase
+	); 
+
+	foreach($methods as $method) {
+		$route[$method] = array($class, $method); // metodo de la clase, ej. post, get, etc.
+	}
+
 	$router->addRoute($route);
 };
 
-$addRoute('/auth', 'Auth', 'post');
+$addRoute('/auth', 'Auth', ['post', 'delete']);
+/* ej.: $addRoute(
+	'/calendar/week/{startDay}',
+	'Week',
+	['get'],
+	array(
+		'startDay' => Zaphpa_Template::regex('\d{4}-\d{2}-\d{2}')
+	)
+);*/
 
 
 try {
   $router->route();
 } catch (Zaphpa_InvalidPathException $ex) {      
   header("Content-Type: application/json;", TRUE, 404);
-  $out = array("error" => "not found");        
-  die(json_encode($out));
+  die(json_encode(array("error" => "not found")));
+  // cualquier otra ruta que no hemos definido: 404 not found
 }
 
 ?>
--- api/routes/Auth.php
+++ api/routes/Auth.php
@@ -1,9 +1,31 @@
 <?php
 
+// insert into usuarios set nombre = 'Benja', email = 'ste3ls@gmail.com', pass = UNHEX(SHA1('<SECRETO>hola'));
+// alter table usuarios modify pass binary(20);
+
 class Auth {
+
 	public function post($req, $res) {
-		$res->add('hola: ' . $req->data['user']);
-		$res->send(201, 'text');
+
+		$users = $GLOBALS['db']->GetAll(
+			'SELECT id, nombre FROM usuarios WHERE UPPER(email) = UPPER(?) AND pass = UNHEX(SHA1(?)) LIMIT 1',
+			[
+				$req->data['user'],
+				$GLOBALS['db']->_salt . $req->data['password']
+			]
+		);
+
+		if (count($users) !== 1) {
+			sleep(1); // pa que no lo peten o algo
+			$res->add('Contraseña o email incorrecto');
+			return $res->send(403, 'text'); // status: 403 Forbidden
+
+		} else {
+			$_SESSION['logged'] = true;
+			$res->add('Acceso correcto'); // no importa, el jQuery solo mira el status
+			return $res->send(200, 'text'); // status: 200 Ok!
+
+		}
 	}
 }
 
