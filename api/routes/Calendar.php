6169b78d987380e0e76b48a52de6c4c8ad0a2c4f
benja <benja@maletin.(none)>
2014-02-12 21:00:58 +0100

    show full classes

--- api/routes/Calendar.php
+++ api/routes/Calendar.php
@@ -22,12 +22,20 @@ class Calendar {
 				'title' => $item['nombre'],
 				'start' => $start,
 				'end' => $end,
-				'isEnroled' => $_SESSION['user']['id'] && $_SESSION['user']['id'] == $item['id_usuario']
+				'isEnroled' => $_SESSION['user']['id'] && $_SESSION['user']['id'] == $item['id_usuario'],
+				'userCount' => (int) $item['usercount'],
+				'isFull' => $item['max_usuario'] >= $item['usercount']
 			);
-		}, $GLOBALS['db']->GetAll('SELECT * FROM evento_calendario LEFT JOIN eventos ON eventos.id = evento_calendario.id_evento LEFT JOIN usuario_evento ON usuario_evento.id_evento = evento_calendario.id_evento AND usuario_evento.id_usuario = ?', [$_SESSION['user']['id']]));
+		}, $GLOBALS['db']->GetAll('SELECT * FROM evento_calendario
+			LEFT JOIN eventos ON eventos.id = evento_calendario.id_evento
+			LEFT JOIN usuario_evento ON usuario_evento.id_evento = evento_calendario.id_evento AND usuario_evento.id_usuario = ?
+			LEFT JOIN (SELECT id_evento, COUNT(id_evento) AS usercount FROM usuario_evento) AS uc ON uc.id_evento = eventos.id
+			WHERE fecha BETWEEN FROM_UNIXTIME(?) AND FROM_UNIXTIME(?)',
+			[$_SESSION['user']['id'], $_GET['start'], $_GET['end']]));
 
 		if ($events === null) {
 			$events = [];
+			$events = $GLOBALS['db']->ErrorMsg();
 		}
 
 		$res->add(json_encode($events));
